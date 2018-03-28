28b465dbead450d1b4454e25fcdf6cf05fce9010
Cemil Tokatlı <cemiltokatli@yahoo.com>
2018-03-28 09:54:10 +0300

    Decode operation added

--- src/main/java/com/cemiltokatli/uricomponent/URIComponent.java
+++ src/main/java/com/cemiltokatli/uricomponent/URIComponent.java
@@ -1,6 +1,7 @@
 package com.cemiltokatli.uricomponent;
 
 import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.util.Map;
 
@@ -25,6 +26,22 @@ public abstract class URIComponent {
             encodedComponent = encodedComponent.replaceAll("\\+","%20");
 
             return encodedComponent;
+        }
+        catch(UnsupportedEncodingException e){
+            return component;
+        }
+    }
+
+    /**
+     * Decodes the given component.
+     *
+     * @param component Component string to be decoded
+     * @return the decoded component
+     */
+    public static String decode(String component){
+        try{
+            String decodedComponent = URLDecoder.decode(component, "utf-8");
+            return decodedComponent;
         }
         catch(UnsupportedEncodingException e){
             return component;
