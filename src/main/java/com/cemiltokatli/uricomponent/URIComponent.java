79c67c97b5852ac6b3af606b23b16325aeed286a
Roberto Graham <rgraham@sis.tv>
2018-04-14 19:56:54 +0100

    Java 8 support

--- src/main/java/com/cemiltokatli/uricomponent/URIComponent.java
+++ src/main/java/com/cemiltokatli/uricomponent/URIComponent.java
@@ -3,7 +3,11 @@ package com.cemiltokatli.uricomponent;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.net.URLEncoder;
+import java.util.AbstractMap.SimpleEntry;
+import java.util.Collections;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Provides methods for encoding or decoding URI components.
@@ -19,7 +23,13 @@ public abstract class URIComponent {
     public static String encode(String component){
         try{
             String encodedComponent = URLEncoder.encode(component, "utf-8");
-            Map<String, String> nonEncodedChars = Map.of("~", "%7E", "!","%21","(","%28",")","%29","\'","%27");
+            Map<String, String> nonEncodedChars = Collections.unmodifiableMap(Stream.of(
+                    new SimpleEntry<>("~", "%7E"),
+                    new SimpleEntry<>("!", "%21"),
+                    new SimpleEntry<>("(", "%28"),
+                    new SimpleEntry<>(")", "%29"),
+                    new SimpleEntry<>("\'", "%27"))
+                    .collect(Collectors.toMap(SimpleEntry::getKey, SimpleEntry::getValue)));
             for(Map.Entry<String, String> c : nonEncodedChars.entrySet()){
                 encodedComponent = encodedComponent.replaceAll(c.getValue(), c.getKey());
             }
